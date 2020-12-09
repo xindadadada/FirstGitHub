50bde5d248c1b59953d4c5d14bb2326610081d2e
xindadadada <68638353+xindadadada@users.noreply.github.com>
2020-12-09 20:24:29 +0800

    Add files via upload

---
+++ UUIDDemo.java
@@ -0,0 +1,14 @@
+import java.util.UUID;
+
+public class UUIDDemo {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        for (int i = 0;i <500; i++){
+
+            System.out.println(UUID.randomUUID().toString().replace("-",""));
+        }
+    }
+}
